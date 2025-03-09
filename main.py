52eb1c3f5501fcf04dacf5dd25ed1a6fe8ff5463
Dully <133727755+DullSmallmega176@users.noreply.github.com>
2025-03-09 00:14:14 -0600

    oversight

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ else:
     os.system("clear")
 BONDCOST = [0, 10, 40, 200, 750, 4000, 15000, 60000, 270000, 450000, 1200000, 2000000, 4000000, 7000000, 15000000, 120000000, 450000000, 1900000000, 7500000000, 15000000000, 475000000000, 4500000000000, 95000000000000, 5000000000000000, 95000000000000000]
 SLOT = min(50, max(1, int(re.sub("[^0-9]", '', input("hive slot: ")))))
-BONDPERC = min(30, max(0, int(re.sub("[^0-9]", '', input("bond percentage: ")))))
+BONDPERC = min(130, max(100, int(re.sub("[^0-9]", '', input("bond percentage: ")))))
 def numbFormat(x):
     if x<1000:
         return str(x)
@@ -13,8 +13,8 @@ def numbFormat(x):
     while x >= 1000 and i < len(a)-1:
         x /= 1000; i+=1
     return f"{x:.2f}{a[i]}"
-print(f"{'Level':<5} - {'Honey Per Bee':<15} - {f'Honey Total (x{SLOT})':<15}")
+print(f"{'Level':<5} - {'Honey Per Bee':<15} - {f'Honey Total (x{SLOT})':<15} - {f'Bond ({BONDPERC})':<5}")
 print("-"*45)
 for lvl in range(1, 26):
-    costBee = BONDCOST[lvl-1]/10*(1-BONDPERC/100)*10000
+    costBee = ((10*BONDCOST[lvl-1])/BONDPERC)*10000
     print(f"{lvl:<5} - {numbFormat(costBee):<15} - {numbFormat(costBee*SLOT):<15}")
